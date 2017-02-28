e52511222fc9e9d9c50a26ecd11778bc8c2723a3
Kostiantyn Shykun <shykunkv@gmail.com>
2017-02-28 21:46:20 +0200

    Update Starter.java
    Magic string fixed

--- src/main/java/org/mdb/starter/Starter.java
+++ src/main/java/org/mdb/starter/Starter.java
@@ -1,7 +1,10 @@
 package org.mdb.starter;
 
 public class Starter {
+    
+    private final static String HELLO_MESSAGE = "Hello NN!";
+    
     public static void main(String[] args) {
-        System.out.println("Hello NN!");
+        System.out.println(HELLO_MESSAGE);
     }
 }
