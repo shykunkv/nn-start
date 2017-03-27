8835b91c737b97f6fa1ba91404a23559c1fcbee0
Kostiantyn Shykun <shykunkv@gmail.com>
2017-03-28 00:46:32 +0300

    Update Starter.java

--- src/main/java/org/mdb/starter/Starter.java
+++ src/main/java/org/mdb/starter/Starter.java
@@ -2,7 +2,7 @@ package org.mdb.starter;
 
 public class Starter {
     
-    private final static String HELLO_MESSAGE = "Hello NN!";
+    private final static String HELLO_MESSAGE = "Guys, maybe you should start project? :)";
     
     public static void main(String[] args) {
         System.out.println(HELLO_MESSAGE);
